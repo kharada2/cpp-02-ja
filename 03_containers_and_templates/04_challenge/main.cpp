6fa78cebf500d40481360a8d2be2044ef7a56782
kharada2 <kota.harada@woven-planet.global>
2024-03-15 16:54:49 +0900

    upsate

--- 03_containers_and_templates/04_challenge/main.cpp
+++ 03_containers_and_templates/04_challenge/main.cpp
@@ -1,13 +1,30 @@
 #include <string>
-
+#include <fstream>
 #include "phone_book.hpp"
 
 int main() {
-  PhoneBook phoneBook1;
-  //   phoneBook1.
-  phoneBook1.addContact("Bob", "090-2222-2222");
-  phoneBook1.addContact("Alice", "080-1111-1111");
+    PhoneBook phoneBook1;
+    
+     // テキスト読み込み
+    std::ifstream file("book.txt"); // テキストファイルを開く
 
+    if (!file.is_open()) { // ファイルが正しく開けない場合
+        std::cerr << "Error opening file." << std::endl;
+        return 1; // エラーコードを返す
+    }
+
+    std::string line;
+    while (std::getline(file, line)){
+      auto pos = line.find(", ");
+      if (pos != std::string::npos){
+        std::string name = line.substr(0, pos); // 名前を取得
+        std::string number = line.substr(pos + 2); // 電話番号を取得（カンマとスペースを除く）
+        phoneBook1.addContact(name, number);
+      }
+    }
+
+
+  // 動作確認
   std::cout << phoneBook1.GetPhoneByName("Alic") << std::endl;
   std::cout << phoneBook1.GetPhoneByName("Bob") << std::endl;
 
